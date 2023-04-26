2673fd3132b4e79d1982be560526f73ce8539a35
Tom Coleman <tom@chromatic.com>
2023-04-26 12:42:20 +1000

    Updated to 7.0.7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,11 @@
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    'storybook-dark-mode',
-  ],
-  framework: '@storybook/react',
-  core: {
-    builder: '@storybook/builder-webpack5',
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', 'storybook-dark-mode', '@storybook/addon-mdx-gfm'],
+  framework: {
+    name: '@storybook/nextjs',
+    options: {}
   },
+  docs: {
+    autodocs: true
+  }
 };
