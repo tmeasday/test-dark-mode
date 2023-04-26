7ba8f7f49b090ff42b86e9dd337ccf188a7bf68e
Tom Coleman <tom@chromatic.com>
2023-04-26 12:43:21 +1000

    Use ssv6

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,20 @@
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', 'storybook-dark-mode', '@storybook/addon-mdx-gfm'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-dark-mode',
+    '@storybook/addon-mdx-gfm',
+  ],
   framework: {
     name: '@storybook/nextjs',
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
+  features: {
+    storyStoreV7: false,
+  },
 };
